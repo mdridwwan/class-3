d6ce7903d5b39c9fb89c6d563f4c9f53001d53f1
mdridwwan <mdridwan583011@gmail.com>
2024-01-29 23:11:56 +0600

    last update

---
+++ margeSort.js
@@ -0,0 +1,29 @@
+function margeSort(arr) {
+  let len = arr.length;
+  if (len < 2) {
+    return arr;
+  }
+
+  const mid = Math.floor(len / 2);
+  const leftArr = arr.slice(0, mid);
+  const rightArr = arr.slice(mid);
+
+  return marge(margeSort(leftArr), margeSort(rightArr));
+}
+
+function marge(leftArr, rightArr) {
+  const sortedArr = [];
+  //   console.log(leftArr);
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] <= rightArr[0]) {
+      sortedArr.push(leftArr.shift());
+    } else {
+      sortedArr.push(rightArr.shift());
+    }
+  }
+  return [...sortedArr, ...leftArr, ...rightArr];
+}
+
+const arr = [10, 20, -20, -5, 2, 5, 1, 100, -200, 30];
+
+console.log(margeSort(arr));
