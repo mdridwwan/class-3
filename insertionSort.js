10d9957e555e9d793a8703fdc2a3845c2de8ef83
mdridwwan <mdridwan583011@gmail.com>
2024-02-11 22:26:54 +0600

    last update

--- insertionSort.js
+++ insertionSort.js
@@ -1,3 +1,10 @@
 //Insertion Algorithm
 // [5,3,6,1,7,9,4];
 // => [1,3,4,5,6,7,9,];
+
+const arr = [5, 3, 6, 1, 7, 9, 4];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(i);
+  while (true) {}
+}
