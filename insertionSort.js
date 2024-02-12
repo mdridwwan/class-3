f99aa38ba37d2db767d089deace43e30bdcc22b0
mdridwwan <mdridwan583011@gmail.com>
2024-02-12 22:17:37 +0600

    last update

--- insertionSort.js
+++ insertionSort.js
@@ -4,7 +4,18 @@
 
 const arr = [5, 3, 6, 1, 7, 9, 4];
 
-for (let i = 0; i < arr.length; i++) {
-  console.log(i);
-  while (true) {}
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let key = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > key) {
+      array[j + 1] = array[j];
+      j = j - 1;
+    }
+    array[j + 1] = key;
+  }
+  return array;
 }
+
+const result = insertionSort(arr);
+console.log(result);
