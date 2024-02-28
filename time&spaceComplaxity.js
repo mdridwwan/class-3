43cbce14ed983540766e73f4641337947ca34571
mdridwwan <mdridwan583011@gmail.com>
2024-02-28 16:37:41 +0600

    last uodate

---
+++ time&spaceComplaxity.js
@@ -0,0 +1,20 @@
+// let first_Number = 10;
+// let second_Number = 20;
+// let result = first_Number + second_Number;
+// // console.log(result);
+// O(1)
+
+// n*(n+1)/2
+
+// let n = 100000000;
+// let result = (n * (n + 1)) / 2;
+// console.log(result);
+
+// //loop
+// let n = 100000000;
+// let result = 0;
+// for (let i = 0; i <= n; i++) {
+//   result = result + i;
+// }
+
+// console.log(result);
