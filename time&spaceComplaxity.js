dcb8f5f908fd2fb3ecc95bf22605235f3882bf76
mdridwwan <mdridwan583011@gmail.com>
2024-03-10 23:00:59 +0600

    last update

--- time&spaceComplaxity.js
+++ time&spaceComplaxity.js
@@ -20,18 +20,34 @@
 // console.log(result);
 //time and space complexity
 
-// const n = 100;
-// const even = new Array(n + 1).fill(false);
-// for (let i = 0; i <= n; i += 2) {
-//   even[i] = true;
-// }
-
-// console.log(even);
-
-const n = 100;
+const n = 10;
 const even = new Array(n + 1).fill(false);
 for (let i = 0; i <= n; i += 2) {
+  console.log(i);
   even[i] = true;
 }
 
 console.log(even);
+
+// const n = 11;
+// const even = [];
+// for (let j = 0; j <= n; j++) {
+//   even.push(false);
+// }
+// for (let i = 0; i <= n; i += 2) {
+//   even[i] = true;
+// }
+
+// if (even[i] % 2 === 1) {
+//   even[i] = false;
+//   console.log(i);
+//   even.push(even[i]);
+//   console.log(even[i]);
+// } else {
+//   even[i] = true;
+//   console.log(i);
+//   even.push(even[i]);
+//   console.log(even[i]);
+// }
+
+// console.log(even);
